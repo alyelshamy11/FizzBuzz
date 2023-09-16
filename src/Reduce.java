8aa99b11c3f2baf7d4a4262b572b2057eaeb11db
Aly Elshamy <alyelshamy@Alys-MacBook-Air.local>
2023-09-16 10:01:19 -0400

    Finished Reduce Hw

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        Reducer(25);
+    }
+
+    public static int Reducer(int n) {
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n--;
+            }
+        }
+        System.out.println(n);
+        return n;
+    }
 }
